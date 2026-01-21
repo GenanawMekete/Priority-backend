2a59633220566c79047189e77c3783a693d4cd09
GenanawMekete <104999951+GenanawMekete@users.noreply.github.com>
2026-01-22 00:20:30 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import { connectDB } from "./config/db.js";
+import { gameSocket, startNewRound } from "./sockets/gameSocket.js";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+await connectDB();
+
+gameSocket(io);
+startNewRound(io);
+
+server.listen(4000, () => console.log("Bingo backend running on 4000"));
