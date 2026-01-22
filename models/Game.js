bfd78707c75ccd4fefe3a768f067146e0c6c45c8
GenanawMekete <104999951+GenanawMekete@users.noreply.github.com>
2026-01-22 18:25:52 +0300

    Update Game.js

--- models/Game.js
+++ models/Game.js
@@ -1,10 +1,43 @@
-  players: [{
-    telegramId: String,
-    cards: [mongoose.ObjectId],
-    hasBingo: { type: Boolean, default: false }
+import mongoose from "mongoose";
+
+const playerSchema = new mongoose.Schema({
+  telegramId: {
+    type: String,
+    required: true
+  },
+  cards: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Card"
   }],
-  calledNumbers: [Number],
-  winners: [String]
+  hasBingo: {
+    type: Boolean,
+    default: false
+  }
 });
 
-export default mongoose.model("Game", GameSchema);
+const gameSchema = new mongoose.Schema({
+  status: {
+    type: String,
+    enum: ["WAITING", "RUNNING", "VERIFY", "FINISHED"],
+    default: "WAITING"
+  },
+
+  players: [playerSchema],
+
+  winners: [{
+    type: String
+  }],
+
+  calledNumbers: [{
+    type: Number
+  }],
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Game = mongoose.model("Game", gameSchema);
+
+export default Game;
