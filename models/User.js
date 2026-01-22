ad3d639e0e55a53fab0bcd874aa77e338e85805c
GenanawMekete <104999951+GenanawMekete@users.noreply.github.com>
2026-01-22 18:41:47 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  telegramId: { type: String, unique: true },
-  balance: { type: Number, default: 10 }, // initial bonus
-  blocked: { type: Boolean, default: false }
+const userSchema = new mongoose.Schema({
+  telegramId: {
+    type: String,
+    unique: true
+  },
+  balance: {
+    type: Number,
+    default: 10   // initial bonus
+  },
+  blocked: {
+    type: Boolean,
+    default: false
+  }
 });
 
-export default mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
